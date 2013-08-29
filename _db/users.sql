26bda6d11a8ae4e73d54ce596c00e7e27c88712b
tmatthewsdev <tmatthewsdev@gmail.com>
2013-08-29 18:10:33 -0400

    sql dump of users table

---
+++ _db/users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `group` int(11) NOT NULL DEFAULT '1',
+  `email` varchar(255) NOT NULL,
+  `last_login` varchar(25) NOT NULL,
+  `login_hash` varchar(255) NOT NULL,
+  `profile_fields` text NOT NULL,
+  `created_at` int(11) NOT NULL DEFAULT '0',
+  `updated_at` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
